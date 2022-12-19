b18d361a7adf89bc4903e36acd45bdeba4c1c055
Tami <tami@pollination.ai>
2022-12-19 09:58:18 +0200

    better guides

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="repo-gist",
-    version="0.1.1",
+    version="0.1.2",
     py_modules=["repoGist"],
     install_requires=[],
     author="Tami",
@@ -10,7 +10,7 @@ setup(
     description="Create a gist from the contents of a repository, easy share with ChatGPT",
     long_description="",
     long_description_content_type="text/markdown",
-    url="https://github.com/fire17/auto-repo-gist",
+    url="https://github.com/fire17/repo-gist",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
